4a0e5d22ca024621629aab89c7b4d872c608c3a3
Marianna <20104120@mail.wit.ie>
2024-09-02 22:44:38 +0100

    added useParams to Details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import {createRoot} from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
-
-
 const App = () => {
-  return ( 
+  return (
     <BrowserRouter>
-      <h1>Adopt Me!</h1>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
       <Routes>
         <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />}/>
+        <Route path="/" element={<SearchParams />} />
       </Routes>
-
-
     </BrowserRouter>
-   );
-}
+  );
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
