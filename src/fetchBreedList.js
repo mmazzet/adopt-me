2d6f5ba7bb656983f69b2f6767ac00da9662c196
Marianna <20104120@mail.wit.ie>
2024-09-04 22:29:04 +0100

    refactoring to use uncontrolled forms

--- src/fetchBreedList.js
+++ src/fetchBreedList.js
@@ -1,13 +1,17 @@
 const fetchBreedList = async ({queryKey}) => {
     const animal = queryKey[1];
 
-    const apiRes = await fetch(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`);
+    if (!animal) return [];
 
-    if (!apiRes.ok){
-        throw new Error(`breeds/${animal} fetch not ok`);
-    }
+  const res = await fetch(
+    `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+  );
 
-    return apiRes.json()
+  if (!res.ok) {
+    throw new Error(`breeds ${animal} fetch not ok`);
+  }
+
+  return res.json();
 }
 
 export default fetchBreedList;
