ae446e92222ddfa49eefd0c36ea43860544e67e3
Marianna <20104120@mail.wit.ie>
2024-09-10 21:20:55 +0100

    added layer utilities and components

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -45,7 +45,7 @@ const SearchParams = () => {
             type="text"
             name="location"
             id="location"
-            className="mb-5 block w-60"
+            className="search-input"
             placeholder="Location"
           />
         </label>
@@ -54,7 +54,7 @@ const SearchParams = () => {
           <select
             id="animal"
             value={animal}
-            className="mb-5 block w-60"
+            className="search-input"
             onChange={(e) => {
               setAnimal(e.target.value);
             }}
@@ -70,7 +70,7 @@ const SearchParams = () => {
           <select
             id="breed"
             disabled={breeds.length === 0}
-            className="mb-5 block w-60 disabled:opacity-50"
+            className="search-input grayed-out-disabled"
             name="breed"
           >
             <option />
